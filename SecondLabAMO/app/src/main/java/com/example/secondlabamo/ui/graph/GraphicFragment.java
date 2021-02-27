b0e51baff8c206f71237df91e38cd06d912d7337
andri <assliusarenko@gmail.com>
2021-02-27 02:42:30 +0200

    added graphic fragment

---
+++ SecondLabAMO/app/src/main/java/com/example/secondlabamo/ui/graph/GraphicFragment.java
@@ -0,0 +1,18 @@
+package com.example.secondlabamo.ui.graph;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import com.example.secondlabamo.R;
+
+
+public class GraphicFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_graph, container, false);
+        return root;
+    }
+}
