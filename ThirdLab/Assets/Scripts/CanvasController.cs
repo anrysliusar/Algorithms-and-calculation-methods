808b009a5b0150f837ce47a996b2e93dea69c636
andri <assliusarenko@gmail.com>
2021-03-16 15:08:42 +0200

    add lab3

---
+++ ThirdLab/Assets/Scripts/CanvasController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using AwesomeCharts;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasController : MonoBehaviour {
+    public static CanvasController instance;
+    public static Interpolation interpolation;
+    public GameObject info;
+    public GameObject myFunc;
+    public GameObject sin;
+    public GameObject error;
+    public Image errorImage;
+    public Text errorText;
+    private Coroutine errorCoroutine;
+
+    public LineChart myFuncPlot;
+    public LineChart myFuncErrorPlot;
+    public LineChart sinPlot;
+    public LineChart sinErrorPlot;
+    public float limLeft;
+    public float limRight;
+
+
+    private void Awake() {
+        instance = this;
+        interpolation = new Interpolation(this);
+    }
+
+    private void Start() {
+        SetDefault();
+        ConfigurePlotChartAxisBorders(myFuncPlot.XAxis, new Vector2(limLeft, limRight));
+        ConfigurePlotChartAxisBorders(myFuncErrorPlot.XAxis, new Vector2(limLeft, limRight)); 
+        ConfigurePlotChartAxisBorders(sinPlot.XAxis, new Vector2(limLeft, limRight));
+        ConfigurePlotChartAxisBorders(sinErrorPlot.XAxis, new Vector2(limLeft, limRight)); 
+    }
+
+    public void SetDefault() {
+        info.SetActive(true);
+        myFunc.SetActive(false);
+        sin.SetActive(false);
+    }
+
+    private void OnClick(GameObject other) {
+        info.SetActive(false);
+        other.SetActive(true);
+    }
+
+    public void OnMyFunc() {
+        OnClick(myFunc);
+        MyFunction();
+    }
+
+    public void OnSin() {
+        OnClick(sin);
+        SinFunction();
+    }
+
+    private void SinFunction() {
+        ClearPlotChart(sinPlot);
+        ClearPlotChart(sinErrorPlot);
+        
+        interpolation.SinFunctionInterpolation(11, limLeft, limRight);
+        interpolation.SinFuncTweak(11, limLeft, limRight);
+
+        RefreshAllPlotCharts();
+    }
+
+    private void MyFunction() {
+        ClearPlotChart(myFuncPlot);
+        ClearPlotChart(myFuncErrorPlot);
+        
+        interpolation.GivenFuncInterpolation(11, limLeft, limRight);
+        interpolation.GivenFuncTweak(11, limLeft, limRight);
+
+        RefreshAllPlotCharts();
+    }
+
+    public void AddEntryToMainPlot(int dataSet, Vector2 entry, LineChart plot) {
+        plot.GetChartData().DataSets[dataSet].AddEntry(new LineEntry(entry.x, entry.y)); 
+    }
+
+    public void AddEntryToErrorPlot(int dataSet, Vector2 entry, LineChart plot) {
+        plot.GetChartData().DataSets[dataSet].AddEntry(new LineEntry(entry.x, entry.y));
+    }
+
+    private void ConfigurePlotChartAxisBorders(AxisBase axisObj, Vector2 axis) {
+        axisObj.MinAxisValue = axis.x;
+        axisObj.MaxAxisValue = axis.y;
+    }
+
+    private void ClearPlotChart(LineChart lineChart) {
+        foreach (var dataSet in lineChart.GetChartData().DataSets) {
+            dataSet.Clear();
+        }
+    }
+
+    private void RefreshAllPlotCharts() {
+        myFuncPlot.SetDirty();
+        myFuncErrorPlot.SetDirty();
+        sinPlot.SetDirty();
+        sinErrorPlot.SetDirty();
+    }
+}
---
+++ ThirdLab/Assets/Scripts/Interpolation.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+
+public class Interpolation {
+    private CanvasController userUI;
+
+    public Interpolation(CanvasController userUI) {
+        this.userUI = userUI;
+    }
+
+    public void SinFunctionInterpolation(int degree, float limLeft, float limRight) {
+        
+        float[] arrX = ValuesInInterval(limLeft, limRight, degree);
+        float[] arrY = new float[arrX.Length];
+        
+        for (int i = 0; i < arrX.Length; i++) {
+            arrY[i] = Mathf.Sin(arrX[i]);
+        }
+
+        float valX = limLeft;
+        float incVal = 0.01f;
+        
+        while (valX < limRight) {
+            float analyticVal = Mathf.Sin(valX);
+            float interpolVal = Interpolate(arrX, arrY, valX);
+            userUI.AddEntryToMainPlot(0, new Vector2(valX, interpolVal), CanvasController.instance.sinPlot);
+            userUI.AddEntryToMainPlot(1, new Vector2(valX, analyticVal), CanvasController.instance.sinPlot);
+            valX += incVal;
+        }
+    }
+
+    public void GivenFuncInterpolation(int degree, float limLeft, float limRight) {
+        float[] arrX = ValuesInInterval(limLeft, limRight, degree);
+        float[] arrY = new float[arrX.Length];
+        for (int i = 0; i < arrX.Length; i++) {
+            arrY[i] = Mathf.Sin(arrX[i]) - 2 * Mathf.Cos(arrX[i]);
+        }
+
+        float valX = limLeft;
+        float incVal = 0.01f;
+        while (valX < limRight) {
+            float analyticVal = Mathf.Sin(valX) - 2 * Mathf.Cos(valX);
+            float interpolVal = Interpolate(arrX, arrY, valX);
+            userUI.AddEntryToMainPlot(0, new Vector2(valX, interpolVal), CanvasController.instance.myFuncPlot);
+            userUI.AddEntryToMainPlot(1, new Vector2(valX, analyticVal), CanvasController.instance.myFuncPlot);
+            valX += incVal;
+        }
+    }
+
+    public void SinFuncTweak(int maxDegree, float limLeft, float limRight) {
+        float[][] arrXs = new float[maxDegree + 1][];
+        float[][] arrYs = new float[maxDegree + 1][];
+
+        for (int i = 0; i < maxDegree + 1; i++) {
+            arrXs[i] = ValuesInInterval(limLeft, limRight, i + 2);
+            arrYs[i] = new float[arrXs[i].Length];
+            for (int j = 0; j < arrXs[i].Length; j++) {
+                arrYs[i][j] = Mathf.Sin(arrXs[i][j]);
+            }
+        }
+
+        for (int i = 0; i < maxDegree - 1; i++) {
+            float valX = limLeft;
+            float incVal = 0.01f;
+            while (valX < limRight) {
+                float yValue = Mathf.Abs(Interpolate(arrXs[i], arrYs[i], valX) -
+                                         Interpolate(arrXs[i + 1], arrYs[i + 1], valX));
+                userUI.AddEntryToErrorPlot(i, new Vector2(valX, yValue), CanvasController.instance.sinErrorPlot);
+                valX += incVal;
+            }
+        }
+    }
+
+    public void GivenFuncTweak(int maxDegree, float limLeft, float limRight) {
+        float[][] arrXs = new float[maxDegree + 1][];
+        float[][] arrYs = new float[maxDegree + 1][];
+
+        for (int i = 0; i < maxDegree + 1; i++) {
+            arrXs[i] = ValuesInInterval(limLeft, limRight, i + 2);
+            arrYs[i] = new float[arrXs[i].Length];
+            for (int j = 0; j < arrXs[i].Length; j++) {
+                arrYs[i][j] = Mathf.Sin(arrXs[i][j]) - 2 * Mathf.Cos(arrXs[i][j]);
+            }
+        }
+
+        for (int i = 0; i < maxDegree - 1; i++) {
+            float valX = limLeft;
+            float incVal = 0.01f;
+            while (valX < limRight) {
+                if (valX > 2 && valX < 2.01) {
+                    var deltaN = Interpolate(arrXs[i], arrYs[i], valX) -
+                                 Interpolate(arrXs[i + 1], arrYs[i + 1], valX);
+                    var deltaExactN = Interpolate(arrXs[i], arrYs[i], valX) -
+                                      (Mathf.Sin(valX) - 2 * Mathf.Cos(valX));
+                    var k = 1 - (deltaExactN / deltaN);
+                    Debug.Log($"deltaN = {deltaN}, deltaExactN = {deltaExactN}, k = {k}");
+                }
+
+                float yValue = Mathf.Abs(Interpolate(arrXs[i], arrYs[i], valX) -
+                                         Interpolate(arrXs[i + 1], arrYs[i + 1], valX));
+                userUI.AddEntryToErrorPlot(i, new Vector2(valX, yValue), CanvasController.instance.myFuncErrorPlot);
+                valX += incVal;
+            }
+        }
+    }
+
+    public float Interpolate(float[] xArray, float[] yArray, float xValue) {
+        float functionValue = 0;
+
+        for (int i = 0; i < xArray.Length; i++) {
+            functionValue += DiffOfX(i, xValue, xArray) * DividedDifference(0, i, xArray, yArray);
+        }
+        return functionValue;
+    }
+
+    private float DividedDifference(int limLeftIndex, int limRightIndex, float[] arrX, float[] arrY) {
+        float divDiff = 0;
+
+        for (int j = limLeftIndex; j <= limRightIndex; j++) {
+            float denominator = 1;
+            for (int i = limLeftIndex; i <= limRightIndex; i++) {
+                if (i == j) {
+                    continue;
+                }
+
+                denominator *= (arrX[j] - arrX[i]);
+            }
+
+            divDiff += arrY[j] / denominator;
+        }
+
+        return divDiff;
+    }
+
+    private float DiffOfX(int countOfX, float valX, float[] arrX) {
+        float diffOfX = 1;
+
+        for (int i = 0; i < countOfX; i++) {
+            diffOfX *= valX - arrX[i];
+        }
+
+        return diffOfX;
+    }
+
+    private float[] ValuesInInterval(float limLeft, float limRight, int countOfVal) {
+        float incVal = Mathf.Abs(limRight - limLeft) / (countOfVal - 1);
+        float[] vals = new float[countOfVal];
+
+        for (int i = 0; i < countOfVal; i++) {
+            vals[i] = limLeft + incVal * i;
+        }
+
+        return vals;
+    }
+}
